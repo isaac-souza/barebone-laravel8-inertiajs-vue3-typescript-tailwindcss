e4db6b370d78ea4f34b845f6a9dfecf883796268
Isaac Souza <isaacsouza17@gmail.com>
2021-10-16 09:00:50 -0300

    Initial commit

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p class="text-xl text-gray-700 font-bold">Isaac</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        
+    })
+</script>
